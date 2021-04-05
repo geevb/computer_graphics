b27b95eb62bc8602e41e69585737ca353057bc93
Getulio Beninca <getuliovb@gmail.com>
2021-04-05 21:02:13 +0200

    Load tank from .obj, display & apply functions on it

--- main.cpp
+++ main.cpp
@@ -1,39 +1,44 @@
 #include <iostream> 
+#include <fstream>
 #include <opencv2/core/core.hpp> 
 #include <opencv2/highgui/highgui.hpp> 
 #include <opencv2/imgproc.hpp>
 #include <vector>
 
-const char* WINDOW_NAME = "ComputacaoGrafica";
+const double pi = 3.14159265359;
+const char* WINDOW_NAME = "Computacao_Grafica";
 const int IMAGE_HEIGHT = 1000;
 const int IMAGE_WIDTH = 1000;
-const double pi = 3.14159265359;
-
 cv::Mat image(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3, cv::Scalar(0, 0, 0));
 
-struct vector2d { double x, y, w; };
-struct rectangle {
-    vector2d p1, p2, p3, p4;
-};
-rectangle rect = {};
-
 struct vector3d { double x, y, z, w; };
+struct coordinate { double x, y; };
 
-struct coordinate { int x, y; };
-coordinate lastClickedCoordinate = { x: 200, y: 200 }; // Default value if no point is clicked in the images
-std::vector<coordinate> coordinates = {};
+template <typename Out>
+void split(const std::string &s, char delim, Out result) {
+    std::istringstream iss(s);
+    std::string item;
+    while (std::getline(iss, item, delim)) {
+        *result++ = item;
+    }
+}
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, std::back_inserter(elems));
+    return elems;
+}
 
-std::vector<coordinate> bresenhamLine(int x, int y, int x2, int y2) {
-    int w = x2 - x;
-    int h = y2 - y;
-    int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+std::vector<coordinate> bresenhamLine(double x, double y, double x2, double y2) {
+    double w = x2 - x;
+    double h = y2 - y;
+    double dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
 
     if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
     if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
     if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
 
-    int longest = abs(w);
-    int shortest = abs(h);
+    double longest = abs(w);
+    double shortest = abs(h);
     if (longest <= shortest) {
         longest = abs(h);
         shortest = abs(w);
@@ -42,8 +47,8 @@ std::vector<coordinate> bresenhamLine(int x, int y, int x2, int y2) {
     }
 
     std::vector<coordinate> coordinates = {};
-    int numerator = longest >> 1;
-    for (int i = 0; i <= longest; i++) {
+    double numerator = (int) longest >> 1;
+    for (double i = 0; i <= longest; i++) {
         if (x < 0 || x >= IMAGE_WIDTH || y < 0 || y >= IMAGE_HEIGHT) {
 			continue;
 		}
@@ -75,7 +80,7 @@ std::vector<std::vector<double>> createIdentityMatrix(int size) {
         matrix[i][i] = 1;
     }
 
-    return matrix; 
+    return matrix;
 }
 
 struct Matrix3D {
@@ -202,91 +207,79 @@ struct Matrix3D {
 		matrix[3][3] = 1;
     }
 	
+    void setSimplePerspective(double d_cp) {
+        matrix[0][0] = 1;
+		matrix[0][1] = 0;
+		matrix[0][2] = 0;
+		matrix[0][3] = 0;
+		
+		matrix[1][0] = 0;
+		matrix[1][1] = 1;
+		matrix[1][2] = 0;
+		matrix[1][3] = 0;
+		
+		matrix[2][0] = 0;
+		matrix[2][1] = 0;
+		matrix[2][2] = 0;
+		matrix[2][3] = 0;
+		
+		matrix[3][0] = 0;
+		matrix[3][1] = 0;
+		matrix[3][2] = 1 / d_cp;
+		matrix[3][3] = 1;
+    }
+
+    Matrix3D multiplyMatrix(Matrix3D multiply) {
+		Matrix3D result;
+	    for (int l = 0; l < 4; l++) {
+	        for (int i = 0; i < 4; i++) {
+	            for (int h = 0; h < 4; h++) {
+	                result.matrix[l][i] += matrix[l][h] * multiply.matrix[h][i];
+	            }
+	        }
+	    }
+
+	    return result;
+    }
+
     vector3d multiplyVector3d(vector3d v) {
-        return {
+        vector3d output = {
             x: v.x * matrix[0][0] + v.y * matrix[0][1] + v.z * matrix[0][2] + v.w * matrix[0][3],
             y: v.x * matrix[1][0] + v.y * matrix[1][1] + v.z * matrix[1][2] + v.w * matrix[1][3],
             z: v.x * matrix[2][0] + v.y * matrix[2][1] + v.z * matrix[2][2] + v.w * matrix[2][3],
             w: v.x * matrix[3][0] + v.y * matrix[3][1] + v.z * matrix[3][2] + v.w * matrix[3][3]
         };
+
+        if (output.w != 0 && output.w != 1) {
+			output.x = output.x / output.w;
+			output.y = output.y / output.w;
+			output.z = output.z / output.w;
+		}
+
+        return output;
     };
 };
+struct GeometricShape3D {
+    std::vector<vector3d> points;
+    void draw(Matrix3D m) {
+        for (vector3d& p : points) {
+            p = m.multiplyVector3d(p);
+        }
 
-struct Triangle3D {
-	vector3d p1;
-	vector3d p2;
-	vector3d p3;
-	
-	void draw(Matrix3D m) {
-		p1 = m.multiplyVector3d(p1);
-		p2 = m.multiplyVector3d(p2);
-		p3 = m.multiplyVector3d(p3);
-		
-        paintCoordinates(image, bresenhamLine(p1.x, p1.y, p2.x, p2.y));
-        paintCoordinates(image, bresenhamLine(p2.x, p2.y, p3.x, p3.y));
-        paintCoordinates(image, bresenhamLine(p3.x, p3.y, p1.x, p1.y));
+        int pointsSize = points.size();
+        for (int i = 0; i < pointsSize; i++) {
+            vector3d current = points[i];
+            vector3d next = ((i + 1) < pointsSize) ? points[i +1] : points[0]; // If next is outside array, get starting point.
+            paintCoordinates(image, bresenhamLine(current.x, current.y, next.x, next.y));
+        }
 	};
 };
-Triangle3D t1;
-Triangle3D t2;
-Triangle3D t3;
-Triangle3D t4;
 
-vector2d multiplyVector2d(vector2d v, std::vector<std::vector<double>> matriz) {
-    return {
-        x: v.x * matriz[0][0] + v.y * matriz[0][1] + v.w * matriz[0][2], 
-        y: v.x * matriz[1][0] + v.y * matriz[1][1] + v.w * matriz[1][2], 
-        w: v.x * matriz[2][0] + v.y * matriz[2][1] + v.w * matriz[2][2]
-    };
-}
+std::vector<GeometricShape3D> tankShapes;
 
-void setTranslate2d(std::vector<std::vector<double>> &matriz, double a, double b) {
-    matriz[0][0] = 1;
-    matriz[0][1] = 0;
-    matriz[0][2] = a;
-    
-    matriz[1][0] = 0;
-    matriz[1][1] = 1;
-    matriz[1][2] = b;
-    
-    matriz[2][0] = 0;
-    matriz[2][1] = 0;
-    matriz[2][2] = 1;
-}
-
-void rotateRectangle(std::vector<std::vector<double>> &matriz, float degree, coordinate pivot) {
-    double rad = (degree * (pi / 180));
-    matriz[0][0] = cos(rad);
-    matriz[0][1] = -sin(rad);
-    matriz[0][2] = (pivot.x * (1 - cos(rad)) + pivot.y * sin(rad));
-    
-    matriz[1][0] = sin(rad);
-    matriz[1][1] = cos(rad);
-    matriz[1][2] = (pivot.y * (1 - cos(rad)) - pivot.x * sin(rad));
-    
-    matriz[2][0] = 0;
-    matriz[2][1] = 0;
-    matriz[2][2] = 1;
-}
-
-void mouseHandler(int event, int x, int y, int flags, void* userdata) {
-    if (event == cv::EVENT_LBUTTONDOWN) {
-        std::cout << "Clicked (" << x << ", " << y << ")" << std::endl;
-
-        lastClickedCoordinate = { x, y };
-        coordinate coord = { x, y };
-        coordinates.push_back(coord);
-
-        int numCoordinates = coordinates.size();
-        if (numCoordinates % 2 == 0) {
-            std::cout << "Gonna create line between" << std::endl;
-            std::cout << "x: " << (coordinates[numCoordinates -1].x) << " y: " << coordinates[numCoordinates -1].y << std::endl;
-            std::cout << "x: " << (coordinates[numCoordinates -2].x) << " y: " << coordinates[numCoordinates -2].y << std::endl;
-            cv::Point p1(coordinates[numCoordinates -1].x, coordinates[numCoordinates -1].y);
-            cv::Point p2(coordinates[numCoordinates -2].x, coordinates[numCoordinates -2].y);
-            paintCoordinates(image, bresenhamLine(p1.x, p1.y, p2.x, p2.y));
-            cv::imshow(WINDOW_NAME, image);
-        }
+void drawTank(Matrix3D m) {
+    for (GeometricShape3D& shape : tankShapes) {
+        shape.draw(m);
     }
 }
 
@@ -294,59 +287,44 @@ void clearImage() {
     image = (IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3, cv::Scalar(0, 0, 0)); // Clear image i.e. sets to black again.
 }
 
-void draw3DTriangle(Matrix3D mat3d) {
-    clearImage();
-
-    t1.draw(mat3d);
-    t2.draw(mat3d);
-    t3.draw(mat3d);
-    t4.draw(mat3d);
-
+void displayImage() { // Turn image upsidedown & display it
+    cv::Mat dst;
+    cv::Point2f pt(image.cols/2., image.rows/2.);
+    cv::warpAffine(image, dst, cv::getRotationMatrix2D(pt, 180, 1.0), cv::Size(image.cols, image.rows));
+    image = dst;
     cv::imshow(WINDOW_NAME, image);
 }
 
-void drawRectangle(rectangle rect) {
-    clearImage();
-    paintCoordinates(image, bresenhamLine(rect.p1.x, rect.p1.y, rect.p2.x, rect.p2.y));
-    paintCoordinates(image, bresenhamLine(rect.p2.x, rect.p2.y, rect.p3.x, rect.p3.y));
-    paintCoordinates(image, bresenhamLine(rect.p3.x, rect.p3.y, rect.p4.x, rect.p4.y));
-    paintCoordinates(image, bresenhamLine(rect.p4.x, rect.p4.y, rect.p1.x, rect.p1.y));
-    cv::imshow(WINDOW_NAME, image);
-}
-
-int waitAndExecutePressedKeyAction() {
+void waitAndExecutePressedKeyAction() {
     char pressedKey = (char) cv::waitKey(0);
     std::cout << "Key pressed: " << pressedKey << std::endl;
-    std::vector<std::vector<double>> identity = createIdentityMatrix(3);
     Matrix3D mat3d;
 
     if (pressedKey == 'c') {
-        return 1;
+        return;
     }
 
     if (pressedKey == 'Q') { // left
-        setTranslate2d(identity, -10, 0);
+
     }
 
     if (pressedKey == 'R') { // up
-        setTranslate2d(identity, 0, -10);
+        
     }
 
     if (pressedKey == 'S') { // right
-        setTranslate2d(identity, 10, 0);
+        
     }
 
     if (pressedKey == 'T') { // down
-        setTranslate2d(identity, 0, 10);
+
     }
 
     if (pressedKey == 'e') {
-        // rotateRectangle(identity, 15, lastClickedCoordinate);
         mat3d.setRotateX(5);
     }
 
     if (pressedKey == 'q') {
-        // rotateRectangle(identity, -15, lastClickedCoordinate);
         mat3d.setRotateX(-5);
     }
 
@@ -366,44 +344,77 @@ int waitAndExecutePressedKeyAction() {
         mat3d.setRotateZ(-5);
     }
     
-    // rect.p1 = multiplyVector2d(rect.p1, identity);
-    // rect.p2 = multiplyVector2d(rect.p2, identity);
-    // rect.p3 = multiplyVector2d(rect.p3, identity);
-    // rect.p4 = multiplyVector2d(rect.p4, identity);
-    // drawRectangle(rect);
-
-    draw3DTriangle(mat3d);
+    clearImage();
+    drawTank(mat3d);
+    displayImage();
 
     return waitAndExecutePressedKeyAction();
 }
 
+class objLoader {
+    struct trigFace { int v1, v2, v3; };
+    struct rectFace { int v1, v2, v3, v4; };
+    public:
+        std::vector<vector3d> vertexes;
+        std::vector<trigFace> trigFaces;
+        std::vector<rectFace> rectFaces;
+        void readfile(std::string filename) {
+            std::ifstream file(filename);
+            if (file.is_open()) {
+                std::string line;            
+                while (std::getline(file, line)) {
+                    if (
+                        line.empty()
+                        || (line.at(0) != 'v' && line.at(0) != 'f')
+                    ) {
+                        continue;
+                    }
+
+                    std::istringstream iss(line);
+                    std::string lineId;
+                    iss >> lineId;
+                    if (lineId == "v") {
+                        vector3d v;
+                        iss >> v.x >> v.y >> v.z;
+                        v.x += IMAGE_WIDTH / 2;
+                        v.y += IMAGE_HEIGHT / 2; // Traz pro centro do imagem
+                        v.z += IMAGE_WIDTH / 2;
+                        v.w = 1;
+                        vertexes.push_back(v);
+                        continue;
+                    }
+
+                    if (lineId == "f") {
+                        std::vector<int> vertexPropsVec;
+                        std::string vertexProps;
+                        while (iss >> vertexProps) {
+                            // use first value in f row, i.e. xx/*ignored* & [0], because that's the vertex index, the others are textures indexes
+                            int vertexIndex = std::stoi(split(vertexProps, '/')[0]) -1; // -1 because .obj uses indexes starting from 1 instead of 0
+                            vertexPropsVec.push_back(vertexIndex);
+                        }
+
+                        GeometricShape3D s;
+                        for (auto& vertexPropIndex : vertexPropsVec) {
+                            s.points.push_back(vertexes[vertexPropIndex]);
+                        }
+                        tankShapes.push_back(s);
+                    }
+                }
+                file.close();
+            }
+        };
+};
+
 int main() {
     cv::namedWindow(WINDOW_NAME);
-    cv::setMouseCallback(WINDOW_NAME, mouseHandler);
 
-    rect = {
-        { 200, 200, 1 },
-        { 300, 200, 1 },
-        { 300, 300, 1 },
-        { 200, 300, 1 }
-    };
-
-    vector3d p1 = { 500, 400, 0, 1 };
-    vector3d p2 = { 600, 500, 0, 1 };
-    vector3d p3 = { 400, 500, 0, 1 };
-    vector3d p4 = { 500, 450, 100, 1 };
-
-    t1 = { p1, p2, p3 };
-    t2 = { p1, p2, p4 };
-    t3 = { p2, p3, p4 };
-    t4 = { p3, p1, p4 };
-
-    // drawRectangle(rect);
+    objLoader obj;
+    obj.readfile("tank.obj");
 
     Matrix3D m;
-    draw3DTriangle(m);
+    drawTank(m);
+    displayImage();
 
     waitAndExecutePressedKeyAction();
-  
     return 0;
 }
